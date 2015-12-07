a136ca3fb1b7f857bdea0a4975ef4d0cc1b40aa3
Grahame Cenkner <grahamecenkner+Git@gmail.com>
2015-12-07 14:45:18 -0700

    binary display

--- ScratchPad/ScratchPad/main.cpp
+++ ScratchPad/ScratchPad/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
 int main()
 {
-	int integerValue;
-	cout << "Type in an int." << endl;
-	cin >> integerValue;
+	cout << "Type in values to see in 128 bit format " << endl << endl;
+	char quitloop = 'n';
+	while (quitloop != 'Y') {
+		float fValue;
+		cin >> fValue;
 
-	cout << "Value: " << integerValue << endl;
+		cout << std::bitset<128>(fValue) << endl << endl;
 
-	system("PAUSE");
+		//cout << "To stop testing type Y: ";
+		//cin >> quitloop;
+	}
 
 	return 0;
 }
